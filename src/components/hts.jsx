3b8afb61a227871a2db354e4e526cfccf031edc5
DESKTOP-K2STHML\Sang Jin <mitotero@kakao.com>
2023-12-26 09:22:16 +0900

    2023.12.26-2

--- src/components/hts.jsx
+++ src/components/hts.jsx
@@ -47,6 +47,42 @@ export default function FundarmentalPage({ swiperRef }) {
         if (page) { fetchData(page); }
     }, [page])
 
+    // 10분 주기 업데이트
+    useEffect(() => {
+        const now = new Date();
+        const hour = now.getHours();
+        const minutes = now.getMinutes();
+        const seconds = now.getSeconds();
+        let delay;
+        if (hour < 9 || (hour === 9 && minutes < 1)) {
+            delay = ((9 - hour - 1) * 60 + (61 - minutes)) * 60 - seconds;
+        } else {
+            // 이미 9시 1분 이후라면, 다음 5분 간격 시작까지 대기 (예: 9시 3분이라면 9시 6분까지 대기)
+            delay = (5 - (minutes - 1) % 10) * 60 - seconds;
+        }
+        // 9시 정각이나 그 이후의 다음 분 시작부터 1분 주기로 데이터 업데이트
+        const startUpdates = () => {
+            const intervalId = setInterval(() => {
+                const now = new Date();
+                const hour = now.getHours();
+                const dayOfWeek = now.getDay();
+                if (dayOfWeek !== 0 && dayOfWeek !== 6 && hour >= 9 && hour < 16) {
+                    fetchData(page);
+                } else if (hour >= 16) {
+                    // 3시 30분 이후라면 인터벌 종료
+                    clearInterval(intervalId);
+                }
+            }, 1000 * 60 * 10);
+            return intervalId;
+        };
+        // 첫 업데이트 시작
+        const timeoutId = setTimeout(() => {
+            startUpdates();
+        }, delay * 1000);
+
+        return () => clearTimeout(timeoutId);
+    }, [])
+
     return (
         <Grid container>
             <Grid item sx={{ pt: 1, pb: 1 }}>
