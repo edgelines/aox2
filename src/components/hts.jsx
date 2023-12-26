70de437ef10a11ee4614c6f2e396619902ef7913
DESKTOP-K2STHML\Sang Jin <mitotero@kakao.com>
2023-12-26 11:27:12 +0900

    .

--- src/components/hts.jsx
+++ src/components/hts.jsx
@@ -155,7 +155,8 @@ const DataTable = ({ swiperRef, data }) => {
                 if (params.value == null) {
                     return '';
                 }
-                return `${params.value.toLocaleString('kr')}`;
+                return `${params.value}`;
+                // return `${params.value.toLocaleString('kr')}`;
             },
         }, {
             field: '대비율', headerName: '등락', width: 45,
@@ -207,7 +208,8 @@ const DataTable = ({ swiperRef, data }) => {
         }
 
         return (
-            <div style={{ color: color }}> {`${params.value.toLocaleString('kr')}`} </div>
+            <div style={{ color: color }}> {`${params.value}`} </div>
+            // <div style={{ color: color }}> {`${params.value.toLocaleString('kr')}`} </div>
         );
     }
 
