fd7862aa674a37196e4af44d8abb204b281f026d
DESKTOP-K2STHML\Sang Jin <mitotero@kakao.com>
2024-05-22 13:50:04 +0900

    Box 위치 살짝 변경

--- src/components/leadThemesTop.jsx
+++ src/components/leadThemesTop.jsx
@@ -163,7 +163,7 @@ export default function LeadThemesTopPage({ swiperRef }) {
                 <Typography sx={{ fontSize: '12px', color: 'tomato' }} >X : 오늘기준</Typography>
             </Box>
 
-            <Box sx={{ position: 'absolute', transform: 'translate(7px, 855px)', zIndex: 0, backgroundColor: 'rgba(0, 0, 0, 0.2)', textAlign: 'left' }}>
+            <Box sx={{ position: 'absolute', transform: 'translate(7px, 865px)', zIndex: 0, backgroundColor: 'rgba(0, 0, 0, 0.2)', textAlign: 'left' }}>
                 {Array.isArray(checkStats.b1_kospi200) && checkStats.b1_kospi200.length > 0 ?
                     <>
                         <Typography sx={{ fontSize: '13px' }} > 코스피200 : {checkStats.now_kospi200.length} / {checkStats.b1_kospi200.length} ({parseInt(checkStats.now_kospi200.length / checkStats.b1_kospi200.length * 100)}%)</Typography>
