8f8c88a97bc1d064980b5baeb7f7eaf6ad96d553
DESKTOP-K2STHML\Sang Jin <mitotero@kakao.com>
2024-06-11 08:53:17 +0900

    라벨링 추가

--- src/components/testPage.jsx
+++ src/components/testPage.jsx
@@ -15,46 +15,55 @@ export default function TestPage({ }) {
     const [message, setMessage] = useState('');
     const [field, setField] = useState('재무'); // 상태 추가
     const [ws, setWs] = useState(null); // 웹소켓 인스턴스를 상태로 관리
-
+    const [dataset, setDataset] = useState(null);
     const handlePage = (event, value) => { if (value !== null) { setField(value); console.log(value) } }
 
+    const fetchData = async () => {
+        const response = await fetch('https://demo-live-data.highcharts.com/population.json');
+        const data = await response.json();
+        setDataset(data);
+    };
+
     useEffect(() => {
-        const websocket = new WebSocket(`${API_WS}/TestKeyworld`);
-
-        websocket.onopen = () => {
-            console.log('Connected to the server');
-        };
-
-        websocket.onmessage = (event) => {
-
-            const res = JSON.parse(event.data)
-            console.log(res);
-
-        };
-
-        websocket.onerror = (error) => {
-            console.log('WebSocket error: ', error);
-        };
-
-        setWs(websocket); // 웹소켓 인스턴스를 상태에 저장
-        // console.log(websocket);
-
-        // 컴포넌트 언마운트 시 웹소켓 연결 종료
-        return () => {
-            websocket.close();
-        };
+        fetchData();
     }, []);
+    // useEffect(() => {
+    //     const websocket = new WebSocket(`${API_WS}/TestKeyworld`);
 
-    useEffect(() => {
-        // field 상태가 변경될 때 데이터 전송
-        const sendData = (field) => {
-            if (ws && ws.readyState === WebSocket.OPEN && field) {
-                const data = { key: field };
-                ws.send(JSON.stringify(data));
-            }
-        };
-        sendData(field);
-    }, [ws, field]); // field 또는 ws 상태가 변경될 때마다 실행
+    //     websocket.onopen = () => {
+    //         console.log('Connected to the server');
+    //     };
+
+    //     websocket.onmessage = (event) => {
+
+    //         const res = JSON.parse(event.data)
+    //         console.log(res);
+
+    //     };
+
+    //     websocket.onerror = (error) => {
+    //         console.log('WebSocket error: ', error);
+    //     };
+
+    //     setWs(websocket); // 웹소켓 인스턴스를 상태에 저장
+    //     // console.log(websocket);
+
+    //     // 컴포넌트 언마운트 시 웹소켓 연결 종료
+    //     return () => {
+    //         websocket.close();
+    //     };
+    // }, []);
+
+    // useEffect(() => {
+    //     // field 상태가 변경될 때 데이터 전송
+    //     const sendData = (field) => {
+    //         if (ws && ws.readyState === WebSocket.OPEN && field) {
+    //             const data = { key: field };
+    //             ws.send(JSON.stringify(data));
+    //         }
+    //     };
+    //     sendData(field);
+    // }, [ws, field]); // field 또는 ws 상태가 변경될 때마다 실행
 
     return (
         <Grid container >
@@ -72,7 +81,7 @@ export default function TestPage({ }) {
                 <StyledToggleButton fontSize={'10px'} value="주요">주요제품/주요주주</StyledToggleButton>
             </ToggleButtonGroup> */}
             <Grid item xs={12}>
-                <Chart />
+                <Chart dataset={dataset} />
 
             </Grid>
 
