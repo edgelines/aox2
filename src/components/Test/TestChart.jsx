16ca5eb3c30ad5799f12680aa2281090665fbcc4
Sangjin <edgelines1@gmail.com>
2024-08-19 21:31:06 +0900

    On

--- src/components/Test/TestChart.jsx
+++ src/components/Test/TestChart.jsx
@@ -2,21 +2,20 @@ import React, { useState, useEffect, useRef } from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
-const MotionsChart = ({ dataset, datasetDown, height, post }) => {
+const MotionsChart = ({ dataset, height }) => {
     const chartComponent = useRef(null);
     const [chartOptions, setChartOptions] = useState({
         chart: {
-            type: 'heatmap', height: height ? height - 400 : 600, backgroundColor: 'rgba(255, 255, 255, 0)', zoomType: 'xy', width: 1070, animation: false
+            type: 'heatmap', height: height ? height : 300, backgroundColor: 'rgba(255, 255, 255, 0)', zoomType: 'xy', animation: false
         },
-        // boost: {
-        //     useGPUTranslations: true,
-        //     enabled: true
-        // },
+        boost: {
+            useGPUTranslations: true,
+            enabled: true
+        },
         credits: { enabled: false }, title: { text: null },
         subtitle: { enabled: false },
         navigation: { buttonOptions: { enabled: false } },
         xAxis: {
-            title: { text: post.xAxis, style: { color: '#efe9e9ed' } },
             labels: {
                 style: { color: '#404040', fontSize: '11px' }, formatter: function () {
                     var color = this.value > 0 ? '#FCAB2F' : this.value < 0 ? '#00F3FF' : '#efe9e9ed';
@@ -24,9 +23,9 @@ const MotionsChart = ({ dataset, datasetDown, height, post }) => {
                 }
             },
             gridLineWidth: 0,
+            categories: []
         },
         yAxis: {
-            title: { text: post.yAxis, style: { color: '#efe9e9ed' } },
             labels: {
                 style: { color: '#efe9e9ed', fontSize: '11px' },
                 formatter: function () {
@@ -35,6 +34,7 @@ const MotionsChart = ({ dataset, datasetDown, height, post }) => {
                 }
             },
             gridLineWidth: 0,
+            categories: []
         },
 
         colorAxis: {
@@ -53,48 +53,38 @@ const MotionsChart = ({ dataset, datasetDown, height, post }) => {
             layout: 'vertical',
             margin: 0,
             verticalAlign: 'top',
-            y: 25,
-            symbolHeight: 280
+
+            symbolHeight: 200
         },
         series: [{
-            name: 'Sales per employee',
+            name: '',
             // borderWidth: 0.2,
-            dataLabels: {
-                enabled: true,
-                style: {
-                    color: '#efe9e9ed', // 글자 색상
-                    textOutline: 'none', // 글꼴 테두리 제거
-                    fontSize: '11px' // 필요 시 글꼴 크기 설정
-                }
-                // color: '#000000'
-            }
+            // dataLabels: {
+            //     enabled: true,
+            //     style: {
+            //         color: '#efe9e9ed', // 글자 색상
+            //         textOutline: 'none', // 글꼴 테두리 제거
+            //         fontSize: '11px' // 필요 시 글꼴 크기 설정
+            //     }
+            //     // color: '#000000'
+            // }
         }],
 
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 500
-                },
-                chartOptions: {
-                    yAxis: {
-                        labels: {
-                            format: '{substr value 0 1}'
-                        }
-                    }
-                }
-            }]
-        },
-        tooltip: {
-            split: true, shared: true, crosshairs: true,
-            backgroundColor: 'rgba(255, 255, 255, 0.5)',
-            formatter: function () {
-                return `
-                    ${post.xAxis} : ${this.point.series.chart.xAxis[0].categories[this.point.x]}<br/>
-                    ${post.yAxis} : ${this.point.series.yAxis.categories[this.point.y]}<br/>
-                    ${this.point.value} 개<br/>
-                    `;
-            },
-        },
+        // responsive: {
+        //     rules: [{
+        //         condition: {
+        //             maxWidth: 500
+        //         },
+        //         chartOptions: {
+        //             yAxis: {
+        //                 labels: {
+        //                     format: '{substr value 0 1}'
+        //                 }
+        //             }
+        //         }
+        //     }]
+        // },
+
         // plotOptions: {
         //     scatter: {
         //         // showInLegend: false,
@@ -121,14 +111,27 @@ const MotionsChart = ({ dataset, datasetDown, height, post }) => {
             setChartOptions({
                 series: [{
                     // name: '상승',
-                    // turboThreshold: 0,
+                    turboThreshold: 0,
                     data: dataset.data
                 }],
                 xAxis: {
+                    title: { text: dataset.xAxis, style: { color: '#efe9e9ed' } },
                     categories: dataset.xAxisBins
                 },
                 yAxis: {
+                    title: { text: dataset.yAxis, style: { color: '#efe9e9ed' } },
                     categories: dataset.yAxisBins
+                },
+                tooltip: {
+                    split: true, shared: true, crosshairs: true,
+                    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+                    formatter: function () {
+                        return `
+                            ${dataset.xAxis} : ${this.point.series.chart.xAxis[0].categories[this.point.x]}<br/>
+                            ${dataset.yAxis} : ${this.point.series.yAxis.categories[this.point.y]}<br/>
+                            ${this.point.value} 개<br/>
+                            `;
+                    },
                 },
             })
         }
